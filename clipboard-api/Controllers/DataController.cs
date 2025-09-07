d1943d04e602ae9bcdcdbb4f3cb6657caa2b6f80
Kasey <Kaseyoro@gmail.com>
2025-09-07 19:06:13 +0300

    Added metod in DataController

--- clipboard-api/Controllers/DataController.cs
+++ clipboard-api/Controllers/DataController.cs
@@ -3,9 +3,15 @@ using Microsoft.AspNetCore.Mvc;
 namespace clipboard_api.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/[controller]/[action]")]
 public class DataController : ControllerBase
 {
     [HttpGet]
     public IActionResult Get() => Ok("Привет, жопа, Новый год 🎄");
+
+    [HttpGet]
+    public IActionResult UploadPasswords()
+    {
+        return Ok(1 + 1);
+    }
 }
